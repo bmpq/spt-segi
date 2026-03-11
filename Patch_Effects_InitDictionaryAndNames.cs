014e438995a0e0cf7e5f41818964d5c4c73e0b79
bmpq <73074311+bmpq@users.noreply.github.com>
2026-03-11 08:22:54 +0300

    fix smoke effects blocking gi

---
+++ Patch_Effects_InitDictionaryAndNames.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using Systems.Effects;
+using UnityEngine;
+
+namespace tarkin.SEGI.Bep
+{
+    internal class Patch_Effects_InitDictionaryAndNames : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(Effects), nameof(Effects.InitDictionaryAndNames));
+        }
+
+        [PatchPostfix]
+        static void Postfix(Effects __instance)
+        {
+            int newLayerForEffects = 1;
+
+            foreach (var effect in __instance.EffectsArray)
+            {
+                if (effect.BasicParticleSystemMediator != null)
+                {
+                    effect.BasicParticleSystemMediator.gameObject.layer = newLayerForEffects;
+                }
+
+                foreach (var particleSys in effect.Particles)
+                {
+                    if (particleSys.Particle != null)
+                    {
+                        particleSys.Particle.gameObject.layer = newLayerForEffects;
+                    }
+                }
+            }
+        }
+    }
+}
